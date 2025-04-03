cdcbe1fbb06c8a2d04c905c3318eef68cc55e492
mbassi237 <sergeatangana.pablo@gmail.com>
2025-04-03 11:29:49 +0100

    Modifications apportees 03/04/2025 10h30

--- evenements/urls.py
+++ evenements/urls.py
@@ -9,7 +9,7 @@ from .views import (
 
 urlpatterns = [
     # Endpoint pour lister les ressources avec recherche et filtres
-    path('evenements/create/', CreateEventView(), name='event-create'),
+    path('evenements/create/', CreateEventView.as_view(), name='event-create'),
     path('evenements/', EventListView.as_view(), name='event-list'),
     path('evenements/<int:event_id>/', EventDetailView.as_view(), name='event-detail'),
     path('evenements/<int:event_id>/calendar/', AddToCalendarView.as_view(), name='event-add-to-calendar')
